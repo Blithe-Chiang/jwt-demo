a4e718492805a696141e9eb8d5ef90bba2995340
Blithe-Chiang <cn18750441251@126.com>
2021-05-28 09:21:53 +0800

    调整代码结构: 将受到保护的url链接从JwtAuthenticationFilter中分离

--- src/main/java/com/example/demo/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/example/demo/jwt/JwtAuthenticationFilter.java
@@ -1,7 +1,5 @@
 package com.example.demo.jwt;
 
-import org.springframework.util.AntPathMatcher;
-import org.springframework.util.PathMatcher;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -9,45 +7,24 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
-    private static final PathMatcher pathMatcher = new AntPathMatcher();
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
         try {
-            // 如果请求的资源是被保护的url的话，获取 request header中的 authorization。
-            if (isProtectedUrl(request)) {
-//                System.out.println(request.getMethod());
-                if (!request.getMethod().equals("OPTIONS"))
-                    // 在 request 的 header 中添加更多的信息
-                    request = JwtUtil.validateTokenAndAddUserIdToHeader(request);
-            }
+
+            if (!request.getMethod().equals("OPTIONS"))
+                // 在 request 的 header 中添加更多的信息
+                request = JwtUtil.validateTokenAndAddUserIdToHeader(request);
+
         } catch (Exception e) {
             response.sendError(HttpServletResponse.SC_UNAUTHORIZED, e.getMessage());
             return;
         }
         filterChain.doFilter(request, response);
-    }
-
-    private boolean isProtectedUrl(HttpServletRequest request) {
-        List<String> protectedPaths = new ArrayList<String>();
-
-        // 添加保护路径
-        protectedPaths.add("/hello");
-
-        boolean bFind = false;
-        for (String passedPath : protectedPaths) {
-            bFind = pathMatcher.match(passedPath, request.getServletPath());
-            if (bFind) {
-                break;
-            }
-        }
-        return bFind;
     }
 
 }
--- src/main/java/com/example/demo/jwt/JwtFilterRegister.java
+++ src/main/java/com/example/demo/jwt/JwtFilterRegister.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.servlet.Filter;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * 将 JwtAuthenticationFilter 注册到 context 中
@@ -13,9 +15,15 @@ import javax.servlet.Filter;
 public class JwtFilterRegister {
     @Bean
     public FilterRegistrationBean<? extends Filter> jwtFilter() {
-        final FilterRegistrationBean<JwtAuthenticationFilter> registrationBean = new FilterRegistrationBean<>();
-        JwtAuthenticationFilter filter = new JwtAuthenticationFilter();
-        registrationBean.setFilter(filter);
-        return registrationBean;
+        final FilterRegistrationBean<JwtAuthenticationFilter> filter = new FilterRegistrationBean<>();
+        filter.setFilter(new JwtAuthenticationFilter());
+
+        // 设置受到保护的url
+        List<String> protectedUrls = Arrays.asList(
+                "/hello"
+        );
+        filter.setUrlPatterns(protectedUrls);
+
+        return filter;
     }
 }
